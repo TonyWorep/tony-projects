ce3cb914fdc03bff3446eecfb6705a9376a9f390
TonyWorep <tonyworep@gmail.com>
2024-12-22 17:49:06 +0100

    :lipstick: fix the navbar a bit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from "../theme/ModeToggle";
 
 export default function Navbar() {
   return (
-    <header className="flex h-8 my-5 mx-[10%] justify-between">
+    <header className="flex h-8 my-5 w-[75%] mx-auto justify-between">
       <div className="flex items-center gap-5">
         <Link href="/" className="flex gap-3 items-center">
           <Image src={logo} alt="logo" className="w-8" />
