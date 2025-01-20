5d255fc14b64dd5d12ab4537c1635a7c05da62f4
TonyWorep <tonyworep@gmail.com>
2025-01-20 23:30:08 +0100

    :lipstick: fix the display of the names

--- src/components/f1-tic-tac-toe/F1TicTacToeDriverSearch.tsx
+++ src/components/f1-tic-tac-toe/F1TicTacToeDriverSearch.tsx
@@ -19,13 +19,26 @@ export default function F1TicTacToeDriverSearch() {
     const drivers = [...state.gridDriver];
     const position = getPosition(state.card);
     const displayDriver = driver.split(" ");
+    const driverShortHands = [];
 
     if (
       categories[state.categoriesRow[position[0]]].includes(driver) &&
       categories[state.categoriesColumn[position[1]]].includes(driver)
     ) {
       grid[state.card] = state.player ? "X" : "O";
-      drivers[state.card] = `${displayDriver[0][0]}. ${displayDriver[1]}`;
+
+      for (let i = 0; i < displayDriver.length - 1; i++) {
+        if (displayDriver[i] !== displayDriver[i].toLowerCase()) {
+          driverShortHands.push(`${displayDriver[i][0]}.`);
+        } else {
+          driverShortHands.push(displayDriver[i]);
+        }
+      }
+
+      const displayShortHands = driverShortHands.join(" ");
+      drivers[state.card] = `${displayShortHands} ${
+        displayDriver[displayDriver.length - 1]
+      }`;
 
       dispatch({ type: "setGrid", value: grid });
       dispatch({ type: "setGridDriver", value: drivers });
